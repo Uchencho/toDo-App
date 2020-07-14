a1b5e9f8f7f6bcf0c2b175e2466a303779be2c85
Uchencho <aloziekelechi17@gmail.com>
2020-07-14 20:16:09 +0100

    configuration issue

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	// Create a function that returns the string below
+	fmt.Fprint(w, "Hello Todo App")
+}
+
 func main() {
-	// Created the main.go file to initialize the repo
+	// Registering a handler
+	http.HandleFunc("/", hello)
+
+	// Since a handler has been registered above, we can pass
+	// nil as the second arguement to listen and server
+	http.ListenAndServe(":8000", nil)
 }
