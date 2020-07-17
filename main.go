0fa0071a8a02f7fc02f5e8c2e1bc52e6ea321efa
Uchencho <aloziekelechi17@gmail.com>
2020-07-17 14:37:58 +0100

    created a custom logger

--- main.go
+++ main.go
@@ -3,18 +3,24 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+)
+
+type healthJSON struct {
+	Name   string
+	Active bool
+}
+
+var (
+	errorLogger *log.Logger
+	infoLogger  *log.Logger
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	// Create a function that returns the string below
 	fmt.Fprint(w, "Hello Todo App")
-}
-
-type healthJSON struct {
-	Name   string
-	Active bool
 }
 
 func healthcheck(w http.ResponseWriter, req *http.Request) {
@@ -24,6 +30,15 @@ func healthcheck(w http.ResponseWriter, req *http.Request) {
 	}
 	jsonResp, _ := json.Marshal(resp)
 	fmt.Fprint(w, string(jsonResp))
+}
+
+func init() {
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	errorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 func getServerAddress() string {
@@ -40,5 +55,6 @@ func main() {
 
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/healthcheck", healthcheck)
+	infoLogger.Println("Starting the Application...")
 	http.ListenAndServe(getServerAddress(), nil)
 }
